f3d0774a7ac5936c04815192185825911c8e210f
Jabez <zbc0512@163.com>
2023-12-13 14:20:59 +0800

    使用lombok

--- src/main/java/io/zbc/uu/book/entity/Goods.java
+++ src/main/java/io/zbc/uu/book/entity/Goods.java
@@ -1,106 +1,22 @@
 package io.zbc.uu.book.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
 import java.util.Date;
 
+@Data
 public class Goods {
 
     private Integer goodsId;
     private String goodsName;
-    private double goodsPrice;
+    private Double goodsPrice;
     private Integer quantity;
     private String unit;
     private String spec;
     private String type;
     private String supermarket;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date recordDate;
 
-    public Goods() {
-
-    }
-
-    public Goods(String goodsName, double goodsPrice, Integer quantity, String unit, String spec, String type, String supermarket, Date recordDate) {
-        this.goodsName = goodsName;
-        this.goodsPrice = goodsPrice;
-        this.quantity = quantity;
-        this.unit = unit;
-        this.spec = spec;
-        this.type = type;
-        this.supermarket = supermarket;
-        this.recordDate = recordDate;
-    }
-
-    public Integer getGoodsId() {
-        return goodsId;
-    }
-
-    public void setGoodsId(Integer goodsId) {
-        this.goodsId = goodsId;
-    }
-
-    public String getGoodsName() {
-        return goodsName;
-    }
-
-    public void setGoodsName(String goodsName) {
-        this.goodsName = goodsName;
-    }
-
-    public double getGoodsPrice() {
-        return goodsPrice;
-    }
-
-    public void setGoodsPrice(double goodsPrice) {
-        this.goodsPrice = goodsPrice;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public String getSpec() {
-        return spec;
-    }
-
-    public void setSpec(String spec) {
-        this.spec = spec;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getSupermarket() {
-        return supermarket;
-    }
-
-    public void setSupermarket(String supermarket) {
-        this.supermarket = supermarket;
-    }
-
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    public Date getRecordDate() {
-        return recordDate;
-    }
-
-    public void setRecordDate(Date recordDate) {
-        this.recordDate = recordDate;
-    }
 }
